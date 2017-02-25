68e52813c15ad03675441dc3b1219b525e57a878
iannech <ianoniango94@gmail.com>
2017-02-25 11:30:55 +0300

    add changes

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// Server index page
+app.get("/", function(req, res){
+  res.send("Deployed");
+});
+
+// Facebook webhook
+// Used for verification
+app.get("webhook", function(req, res){
+  if(req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN){
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  }else{
+    console.error("verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
